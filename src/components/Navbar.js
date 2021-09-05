4d48743f8fc89f584bebe523f44c1aaa4dd6fa0a
shubhsd <shubhamsd53@gmail.com>
2021-09-05 13:18:06 +0530

    deployment to IP using digital ocean droplet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 // Proptypes check the type of props received and validates if it is correct or not.
 // Default props - if let say value of the written prop is not passed then it will write that default value there instead of showing error
@@ -13,18 +13,18 @@ function Navbar(props) {
         // {/* For dark navbar use class below  */}
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        {/* <li className="nav-item">
-                            <a className="nav-link" href="/about">{props.aboutText}</a>
-                        </li> */}
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">{props.aboutText}</Link>
+                        </li>
                     </ul>
 
                     <form className="d-flex">
